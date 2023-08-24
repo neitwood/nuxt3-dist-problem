bf6aa3faac3724aa281d4ed5b31ed6dd95e1d7bc
Alexey Pilyugin <apilyugin@miatel.ru>
2023-08-24 13:18:39 +0300

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+<!--    <div>-->
+<!--      <form>-->
+<!--        <select v-model="locale">-->
+<!--          <option value="en">en</option>-->
+<!--          <option value="ru">ru</option>-->
+<!--        </select>-->
+<!--        <p>{{ $t('welcome') }}</p>-->
+<!--      </form>-->
+<!--    </div>-->
+    <NuxtLink :to="'store'">Store</NuxtLink>
+<!--    <br>-->
+<!--    <NuxtLink :to="localePath('test', 'ru')">Test in Rus</NuxtLink>-->
+<!--    <br>-->
+<!--    <NuxtLink :to="localePath('/test', 'en')">Test in Eng</NuxtLink>-->
+<!--    <br>-->
+<!--    <NuxtLink :to="localePath('/test')">Test in current locale</NuxtLink>-->
+<!--    <br>-->
+<!--    <NuxtLink to="/test">Test </NuxtLink>-->
+<!--    <h1 class="text-purple-400 bg-black">Main</h1>-->
+<!--    <div class="font-bold">BOLD</div>-->
+<!--    <div class="font-semibold">semi BOLD</div>-->
+<!--    <p>{{ $t('test') }}</p>-->
+<!--    <div class="gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">-->
+<!--      <div v-for="i in 6" class="bg-gray text-4xl">content</div>-->
+<!--    </div>-->
+  </div>
+</template>
+
+<!--<script setup>-->
+<!--  const { locale } = useI18n()-->
+<!--  const localePath = useLocalePath()-->
+<!--  const config = useRuntimeConfig()-->
+<!--  console.log(config.public.BASE_URL)-->
+<!--</script>-->
